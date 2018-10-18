ae99ba25f1d15d71e2c89fb428165095eacf527f
KewenC <KewenC@outlook.com>
2018-10-18 12:53:34 +0800

    更新

--- app/src/main/java/com/kewenc/noti/activity/SearchActivity.java
+++ app/src/main/java/com/kewenc/noti/activity/SearchActivity.java
@@ -2,14 +2,38 @@ package com.kewenc.noti.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.kewenc.noti.R;
+import com.kewenc.noti.dao.NotiDao;
 
-public class SearchActivity extends AppCompatActivity {
+public class SearchActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private EditText etWord;
+    private ImageView imgSearch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+        etWord = findViewById(R.id.etWord);
+        imgSearch = findViewById(R.id.imgSearch);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.imgSearch:
+                String word = etWord.getText().toString();
+                if (word.equals("")) return;
+
+
+                NotiDao notiDao = new NotiDao(this);
+
+                break;
+        }
     }
 }
